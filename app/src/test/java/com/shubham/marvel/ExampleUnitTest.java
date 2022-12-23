cea45c9ea884d955c79f814e6b98b65cedbeaa42
shubham <shubham@alveo.fit>
2022-12-23 12:53:25 +0530

    feat: unit test cases added

--- app/src/test/java/com/shubham/marvel/ExampleUnitTest.java
+++ app/src/test/java/com/shubham/marvel/ExampleUnitTest.java
@@ -1,8 +1,16 @@
 package com.shubham.marvel;
 
+import static com.shubham.marvel.utils.AppConstants.API_KEY;
+import static com.shubham.marvel.utils.AppConstants.GET_COMICS_DATE;
+import static com.shubham.marvel.utils.AppConstants.GET_SIMPLE_DATE;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +21,31 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void checkAPIKeyAvailableOrNot() {
+        if (!API_KEY.equals("")) {
+            System.out.println("API_KEY " + API_KEY);
+        }
+        else {
+            System.out.println("API_KEY not found");
+        }
+    }
+
+    @Test
+    public void convertDateFormat() {
+        String newTime;
+        SimpleDateFormat actual = new SimpleDateFormat(GET_COMICS_DATE);
+        SimpleDateFormat target = new SimpleDateFormat(GET_SIMPLE_DATE);
+        Date date;
+        try {
+            date = actual.parse("2022-03-03");
+            newTime = target.format(date);
+            System.out.println("newTime " + newTime);
+        } catch (ParseException e) {
+            System.out.println("exception caught exception");
+            e.printStackTrace();
+        }
     }
 }
